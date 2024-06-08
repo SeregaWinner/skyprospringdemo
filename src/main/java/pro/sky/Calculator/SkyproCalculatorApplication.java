4e10e9d2913e59a3def16e23340c3657df2fb87a
user <Seregawinner92@gmail.com>
2024-06-08 13:33:46 +0300

    line alignment

--- src/main/java/pro/sky/Calculator/SkyproCalculatorApplication.java
+++ src/main/java/pro/sky/Calculator/SkyproCalculatorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SkyproCalculatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkyproCalculatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproCalculatorApplication.class, args);
+    }
 
 }
